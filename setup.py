fdf801ead022631d1a7f9420f6d6d1315e1fde5f
Melvine Nargeot <melvine.nargeot@gmail.com>
2024-11-02 22:43:07 +0100

    Big commit 2

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[],
 )
