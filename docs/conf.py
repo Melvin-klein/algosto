c7b4d7ba2e9922acfaff9f355f96e0ff9e4cd3a8
Melvine Nargeot <melvine.nargeot@gmail.com>
2024-11-02 23:15:13 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Algosto'
 copyright = '2024, Melvine Nargeot'
 author = 'Melvine Nargeot'
-release = '0.0.13'
+release = '0.0.14'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
