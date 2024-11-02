10ab31cce20b98d51eaccdee0994d0d24b9210fe
Melvine Nargeot <melvine.nargeot@gmail.com>
2024-11-02 22:44:47 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="algosto",
-    version="0.0.12",
+    version="0.0.13",
     author="Melvine Nargeot",
     author_email="melvine.nargeot@gmail.com",
     description="Algosto implements stochastic optimization algorithms.",
